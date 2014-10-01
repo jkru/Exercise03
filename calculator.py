99c02bd849daed56d7ee5a98e2cc11abe1551f61
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 12:14:16 -0700

    completed exercise awaiting code review.

--- calculator.py
+++ calculator.py
@@ -7,14 +7,35 @@ calculator program yourself in this file.
 import arithmetic
 
 def main():
-    while True:
-        user_input = raw_input("enter your function and numbers RPN style")
-        tokens = user_input.split()
-        print(user_input)
-
-
     # Your code goes here
-    pass
+
+    while True:
+        user_input = raw_input("enter your function and numbers RPN style ")
+        token = user_input.split()
+        command = token[0]
+        if command == "q":
+            break
+ 
+        if command == "+":
+            print(arithmetic.add(float(token[1]), float(token[2])))
+        elif command == "-":
+            print(arithmetic.subtract(float(token[1]), float(token[2])))
+        elif command == "*":
+            print(arithmetic.multiply(float(token[1]), float(token[2])))
+        elif command == "/":
+            print(arithmetic.divide(float(token[1]), float(token[2])))
+        elif command == "square":
+            print(arithmetic.square(float(token[1])))
+        elif command == "cube":
+            print(arithmetic.cube(float(token[1])))
+        elif command == "pow":
+            print(arithmetic.power(float(token[1]), float(token[2])))
+        elif command == "mod":
+            print(arithmetic.mod(float(token[1]), float(token[2])))
+        else:
+            print "I don't understand."
+
+
 
 
 if __name__ == '__main__':
